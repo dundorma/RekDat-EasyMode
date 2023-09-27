4b128585f61782e6d6c1077dc5eeea65c78aa688
dundorma <90293691+dundorma@users.noreply.github.com>
2023-09-27 23:43:30 +0700

    rekdat is now easy 😁👍🏿

---
+++ contentScripts.js
@@ -0,0 +1,6 @@
+const spanCollection = document.getElementsByTagName("span");
+
+for (let i = 0; i < spanCollection.length; i++) {
+  const spanElement = spanCollection[i];
+  spanElement.classList = [];
+}
